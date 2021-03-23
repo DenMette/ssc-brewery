b376eab35e0ba7dda944f679579bf94c82e03454
Maarten Casteels <denmette@gmail.com>
2021-03-23 09:09:43 +0100

    Adding some additional data into the system.

--- src/main/java/guru/sfg/brewery/repositories/security/RoleRepository.java
+++ src/main/java/guru/sfg/brewery/repositories/security/RoleRepository.java
@@ -3,8 +3,11 @@ package guru.sfg.brewery.repositories.security;
 import guru.sfg.brewery.domain.security.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 /**
  * @author Maarten Casteels
  */
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
 }
